6fcb24d9641c3f029e13c222403ae9583243e8ec
Egor <egor.osokin7@gmail.com>
2024-04-10 22:55:30 +0300

    chore: remove import from module, use abs path

--- graphql_sync_dataloaders/__init__.py
+++ graphql_sync_dataloaders/__init__.py
@@ -1,11 +1,11 @@
-from .execution_context import DeferredExecutionContext
-from .sync_dataloader import SyncDataLoader
-from .sync_future import SyncFuture, InvalidStateError
+# from .execution_context import DeferredExecutionContext
+# from .sync_dataloader import SyncDataLoader
+# from .sync_future import SyncFuture, InvalidStateError
 
 
-__all__ = [
-    "DeferredExecutionContext",
-    "SyncDataLoader",
-    "SyncFuture",
-    "InvalidStateError",
-]
+# __all__ = [
+#     "DeferredExecutionContext",
+#     "SyncDataLoader",
+#     "SyncFuture",
+#     "InvalidStateError",
+# ]
